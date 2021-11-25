72608f2f75fed5cb86a73a36c43b9507f7769965
tandy <tandy1000@cock.li>
2021-11-25 18:57:04 +0000

    adjust for `getMove` change

--- tests/test.nim
+++ tests/test.nim
@@ -173,7 +173,7 @@ suite "Board":
 
   test "Game Over (true, human winner)":
     board.move(board.nextSquare(0, 1, Direction.southEast), board.grid)
-    board.move(board.getMove(0, 3, Direction.southWest, board.grid).get(), board.grid)
+    board.move(board.getMove(0, 3, Direction.southWest, board.grid)[0], board.grid)
     let (gameOver, winner) = board.isGameOver(board.grid)
     check gameOver == true and winner == some board.human
 
