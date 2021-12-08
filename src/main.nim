418eb0779d378a07c07d098a0cc3ca286379fe9a
tandy <tandy1000@cock.li>
2021-12-08 13:00:11 +0000

    move drawBoard to class

--- src/main.nim
+++ src/main.nim
@@ -11,66 +11,6 @@ var c = newCheckers(difficulty = Difficulty.medium)
 proc gameInit*() =
   loadFont(0, "font.png")
 
-proc gameDraw*() =
-  # c.started = true
-  if not c.started:
-    c.drawStartPage()
-    c.drawHelpButton()
-    c.displayRules()
-  else:
-    cls()
-    setColor(7)
-    printc("CHECKERS", screenWidth div 2, 8)
-
-    var square: Square
-    for x in 0 ..< c.board.dimension:
-      for y in 0 ..< c.board.dimension:
-        square = c.gridBounds[x][y]
-        if c.board.grid[x][y].color == GridColor.light:
-          setColor(7)
-          rectfill(square.x, square.y, square.x1, square.y1)
-        else:
-          setColor(3)
-          rectfill(square.x, square.y, square.x1, square.y1)
-
-        if c.board.grid[x][y].potential:
-          if c.board.grid[x][y].piece.isNone():
-            c.drawPiece(newPiece(c.board.turn), square)
-          elif c.selected.isSome():
-            c.drawPiece(newPiece(c.board.turn, king = c.board.grid[
-                c.selected.get().x][c.selected.get().y].piece.get().king), square)
-        elif c.board.grid[x][y].clue:
-          c.drawPiece(newPiece(c.board.turn), square, clue = true)
-        else:
-          if c.board.grid[x][y].piece.isSome():
-            c.drawPiece(c.board.grid[x][y].piece.get(), square)
-
-    c.drawHelpButton()
-
-    if c.outOfBounds:
-      setColor(4)
-      printc("Please click within the grid!", screenWidth div 2, 246)
-
-    if not c.successfulMove:
-      setColor(4)
-      printc("Try another position!", screenWidth div 2, 246)
-
-    if c.board.turn == c.board.human:
-      setColor(8)
-      printc("Your turn.", screenWidth div 2, 20)
-    elif c.board.turn == c.board.ai:
-      setColor(9)
-      printc("AI's turn.", screenWidth div 2, 20)
-
-    if c.board.gameOver:
-      if c.board.gameResult.isSome():
-        if c.board.gameResult.get() == c.board.human:
-          c.gameOverMessage("You Win!", 3)
-        elif c.board.gameResult.get() == c.board.ai:
-          c.gameOverMessage("You Lose!", 4)
-      else:
-        c.gameOverMessage("Game Over.", 4)
-
 proc gameUpdate*(dt: float32) =
   var
     pos: (int, int)
@@ -107,6 +47,7 @@ proc gameUpdate*(dt: float32) =
     c.cleanGrid()
     c.board.update(c.board)
     (c.board.gameOver, c.board.gameResult) = c.board.isGameOver(c.board)
+
     ## Human turn
     if c.board.turn == c.board.human and c.board.gameOver == false:
       pos = mouse()
@@ -115,8 +56,8 @@ proc gameUpdate*(dt: float32) =
       if c.showHints:
         if not c.isOutOfBounds(pos, c.gridSquare) and c.selected.isSome():
           let (x, y) = c.xyToGrid(pos)
-          if c.board.grid[x][y].color == GridColor.dark and c.board.grid[x][
-              y].piece.isNone():
+          if c.board.grid[x][y].color == GridColor.dark and
+            c.board.grid[x][y].piece.isNone():
             let potentialMoves = c.board.getMoves(c.selected.get().x,
                 c.selected.get().y, c.board.grid)
             if newMove(c.selected.get().x, c.selected.get().y, x, y) in potentialMoves:
@@ -141,8 +82,45 @@ proc gameUpdate*(dt: float32) =
           c.select c.xyToGrid(pos)
           c.cleanGrid(clue = true)
     elif c.board.turn == c.board.ai and c.board.gameOver == false:
-      c.board.moveAI()
+      c.board.moveAI(c.board.ai)
 
+proc gameDraw*() =
+  # c.started = true
+  if not c.started:
+    c.drawStartPage()
+    c.drawHelpButton()
+    c.displayRules()
+  else:
+    cls()
+    setColor(7)
+    printc("CHECKERS", screenWidth div 2, 8)
+
+    c.drawBoard()
+    c.drawHelpButton()
+
+    if c.outOfBounds:
+      setColor(4)
+      printc("Please click within the grid!", screenWidth div 2, 246)
+
+    if not c.successfulMove:
+      setColor(4)
+      printc("Try another position!", screenWidth div 2, 246)
+
+    if c.board.turn == c.board.human:
+      setColor(8)
+      printc("Your turn.", screenWidth div 2, 20)
+    elif c.board.turn == c.board.ai:
+      setColor(9)
+      printc("AI's turn.", screenWidth div 2, 20)
+
+    if c.board.gameOver:
+      if c.board.gameResult.isSome():
+        if c.board.gameResult.get() == c.board.human:
+          c.gameOverMessage("You Win!", 3)
+        elif c.board.gameResult.get() == c.board.ai:
+          c.gameOverMessage("You Lose!", 4)
+      else:
+        c.gameOverMessage("Game Over.", 4)
 
 nico.init(orgName, appName)
 nico.createWindow(appName, 256, 256, 4, false)
